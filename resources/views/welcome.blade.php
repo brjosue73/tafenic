1dbdf016e71bb1f3da0b8483af3999dd707fff69
J-Alex <jlex.wd@outlook.com>
2016-03-13 13:00:28 -0600

    anexo al commit anterior

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="res/dependencies/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="res/css/styles.css">
     </head>
-    <body>
+    <body data-ng-app="moduleName">
+      <main data-ng-controller="controllerName">
+      <a data-ng-href="#/admin/personas" data-ng-click="gotoPersonas()">Personas</a>
+      <a data-ng-href="#/admin/listar" data-ng-click="read()">Listar</a>
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
             </div>
         </div>
+        <section data-ng-view=""></section>
+      </main>
+      <script src="res/dependencies/jquery/dist/jquery.min.js"></script>
+      <script src="res/dependencies/angular/angular.min.js"></script>
+      <script src="res/dependencies/angular-route/angular-route.min.js"></script>
+      <script src="res/dependencies/angular-resource/angular-resource.min.js"></script>
+      <script src="res/js/angular.js"></script>
+      <script src="res/dependencies/bootstrap/dist/js/bootstrap.min.js"></script>
+      <script src="res/js/scripts.min.js"></script>
     </body>
 </html>
