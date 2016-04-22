eba33b5f27a834be44e7e73e357dd17c3ede8d7b
J-Alex <jlex.wd@outlook.com>
2016-04-22 11:15:03 -0600

    refactoring angularScrit

--- public/res/js/angular.js
+++ public/res/js/angular.js
@@ -1,151 +1,40 @@
-
 (function(){
 	'use strict';
-
-	//Create a new Angular module
+	/*******************************************************************************************************************\
+		Create a new angular module
+	\*******************************************************************************************************************/
 	angular.module('moduleName',["ngRoute","ngResource","ui.router"], function($interpolateProvider) {
         $interpolateProvider.startSymbol('<%');
         $interpolateProvider.endSymbol('%>');
   });
-
-	//Use an exist Angular Module
+	/*******************************************************************************************************************\
+		Use an exist Angular Module
+	\*******************************************************************************************************************/
 	var app = angular.module('moduleName');
-
-	//Service Factory resource
+	/*******************************************************************************************************************\
+		Service Factory resources REST with Backend
+	\*******************************************************************************************************************/
 	app.factory('Resource', ['$resource', function(r){
 		return r('trabajadores/:id',{id:"@id"},{update:{method:"PUT"}});
 	}]);
-	/*app.factory('Resource', ['$resource', function(r){
-		return r('trabajadores/editar/:id',{id:"@id"},{update:{method:"PUT"}});
-	}]);*/
-	//Service Factory resource
 	app.factory('fincaResource', ['$resource', function(r){
 	  return r('fincas/:id',{id:"@id"},{update:{method:"PUT"}});
 	}]);
-	//Service Factory resource
 	app.factory('actividadResource', ['$resource', function(r){
 	  return r('actividad/:id',{id:"@id"},{update:{method:"PUT"}});
 	}]);
-	//Service Factory resource
 	app.factory('laborResource', ['$resource', function(r){
 	  return r('labor/:id',{id:"@id"},{update:{method:"PUT"}});
 	}]);
 	app.factory('prepResource', ['$resource', function(r){
 		return r('preplanilla/:id',{id:"@id"},{update:{method:"PUT"}});
-	}])
-	//Create and append a new cotroller for your module
-	app.controller("fincaController",['$scope','$http','fincaResource', function(s,h,fr){
-		var $btnFAceptar = $('#fincAceptar') ;
-	  s.fincaSaveData = {};
-s.fincas = fr.query();
-	  s.fincaSave = function(){
-	    console.log(s.fincaSaveData);
-
-			$('#fincaSpinner').css("display", "inline-block");
-
-			fr.save({data:s.fincaSaveData}, function(res) {
-				console.log(res);
-				$('#fincaSpinner').css("display", "none");
-				$('#exitofinca').css("display","inline");
-				setTimeout(function(){
-					$('#exitofinca').css("display","none");
-				},3000)
-			},function(err){
-				console.log(err.status);
-				$('#fincaSpinner').css("display", "none");
-				$('#errorfinca').css("display","inline");
-				setTimeout(function(){
-					$('#errorfinca').css("display","none");
-				},3000)
-			});
-	  }
 	}]);
-	//Create and append a new cotroller for your module
-	app.controller("actividadController",['$scope','$http','actividadResource','fincaResource', function(s,h,ar,fr){
-	  s.actividadSaveData = {};
-
-		/*var dataEntrante = fr.query();
-		s.lasfincas = dataEntrante;
-
-		console.log(dataEntrante);
-		console.log(s.lasfincas);*/
-
-		/*s.obtener = function(){
-			console.log("actividades");
-			// dataEntrante = fr.query();
-			// s.lasfincas = dataEntrante;
-			h.get('/fincas')
-			.success(function(data){
-				console.log(data);
-				s.lasfincas = data;
-			})
-			.error(function(err){
-				console.log(err);
-			});
-		}*/
-		s.lasfincas = fr.query();
-	  s.actividadSave = function(){
-	    console.log(s.actividadSaveData);
-	    ar.save({data:s.actividadSaveData});
-	  }
-	}]);
-	//Create and append a new cotroller for your module
-	app.controller("laborController",['$scope','$http','laborResource','actividadResource', function(s,h,lr,ar){
-	  s.laborSaveData = {};
-
-		var dataEntra = ar.query();
-		s.lasactividades = dataEntra;
-		s.laslabores=lr.query();
-		s.obtener = function(){
-			console.log("labores");
-			s.lasactividades = "";
-			dataEntra = "";
-		}
-	  s.laborSave = function(){
-	    console.log(s.laborSaveData);
-	    lr.save({data:s.laborSaveData});
-	  }
-	}]);
-	//angular routes config
-	/*app.config(function ($routeProvider) {
-	$routeProvider
-	    .when('/', {
-	      templateUrl: 'partials/loggin.html',
-	      controller: '',
-	      controllerAs: ''
-	    })
-	    .when('/adminPane', {
-	      templateUrl: 'partials/adminPane.html',
-	      controller: '',
-	      controllerAs: ''
-	    })
-	    .when('/trabajadores/nuevo/', {
-	      templateUrl: 'partials/trabajadores/actualizarT.html',
-	      controller: 'postOne',
-	      controllerAs: 'CR0'
-	    })
-	    .when('/trabajadores', {
-	      templateUrl: 'partials/trabajadores/listarT.html',
-	      controller: 'getAll',
-	      controllerAs: 'CR1'
-	    })
-	    .when('/trabajador/editar/:id', {
-	      templateUrl: 'partials/trabajadores/actualizarT.html',
-	      controller: 'getOne',
-	      controllerAs: 'CR2'
-	    })
-	    .when('/preplanilla', {
-	      templateUrl: 'partials/preplanillas/prepPanel.html',
-	      controller: 'preplanilla',
-	      controllerAs: 'pplla'
-	    })
-	    .otherwise({
-	      redirectTo: '/'
-	    });
-	});*/
+	/*******************************************************************************************************************\
+		App Routing
+	\*******************************************************************************************************************/
 	/*UI-ROUTER*/
 	app.config(function($stateProvider, $urlRouterProvider) {
-  	$urlRouterProvider.otherwise("/");
+		$urlRouterProvider.otherwise("/");
 
 		$stateProvider
 		.state('/', {
@@ -181,7 +70,84 @@ s.fincas = fr.query();
 			templateUrl: "partials/fincas/fincasPane.html",
 			controller:"fincaController"
 		})
-  });
+	});
+	/*******************************************************************************************************************\
+		Create and append a new cotrollers for your exist module in use
+	\*******************************************************************************************************************/
+	//Finca Controller
+	app.controller("fincaController",['$scope','$http','fincaResource', function(s,h,fr){
+		var $btnFAceptar = $('#fincAceptar') ;
+	  s.fincaSaveData = {};
+		s.fincas = fr.query();
+	  s.fincaSave = function(){
+	  console.log(s.fincaSaveData);
+
+			$('#fincaSpinner').css("display", "inline-block");
+
+			fr.save({data:s.fincaSaveData}, function(res) {
+				console.log(res);
+				$('#fincaSpinner').css("display", "none");
+				$('#exitofinca').css("display","inline");
+				setTimeout(function(){
+					$('#exitofinca').css("display","none");
+				},3000)
+			},function(err){
+				console.log(err.status);
+				$('#fincaSpinner').css("display", "none");
+				$('#errorfinca').css("display","inline");
+				setTimeout(function(){
+					$('#errorfinca').css("display","none");
+				},3000)
+			});
+	  }
+	}]);
+	//Actividad Controller
+	app.controller("actividadController",['$scope','$http','actividadResource','fincaResource', function(s,h,ar,fr){
+	  s.actividadSaveData = {};
+
+		/*var dataEntrante = fr.query();
+		s.lasfincas = dataEntrante;
+
+		console.log(dataEntrante);
+		console.log(s.lasfincas);*/
+
+		/*s.obtener = function(){
+			console.log("actividades");
+			// dataEntrante = fr.query();
+			// s.lasfincas = dataEntrante;
+			h.get('/fincas')
+			.success(function(data){
+				console.log(data);
+				s.lasfincas = data;
+			})
+			.error(function(err){
+				console.log(err);
+			});
+		}*/
+		s.lasfincas = fr.query();
+	  s.actividadSave = function(){
+	    console.log(s.actividadSaveData);
+	    ar.save({data:s.actividadSaveData});
+	  }
+	}]);
+	//Labores controller
+	app.controller("laborController",['$scope','$http','laborResource','actividadResource', function(s,h,lr,ar){
+	  s.laborSaveData = {};
+
+		var dataEntra = ar.query();
+		s.lasactividades = dataEntra;
+		s.laslabores=lr.query();
+		s.obtener = function(){
+			console.log("labores");
+			s.lasactividades = "";
+			dataEntra = "";
+		}
+	  s.laborSave = function(){
+	    console.log(s.laborSaveData);
+	    lr.save({data:s.laborSaveData});
+	  }
+	}]);
+	/*TRABAJADOR REST CONTROLLERS*/
 	//Create one
 	app.controller('postOne', ['$scope','Resource','$location', function(s,r,l){
 		s.boton = "Guardar";
@@ -193,16 +159,14 @@ s.fincas = fr.query();
 		}
 	}]);
 	//Read one && Update One
-	app.controller('getOne', ['$scope','$routeParams','Resource','$location','$stateParams', function(s,p,r,l,sp){
-		//s.sendData = {};
-		//s.id=sp.id;
+	app.controller('getOne', ['$scope','$stateParams','Resource','$location', function(s,sp,r,l){
+		console.log(sp.id);
 		s.boton="Editar";
-		//console.log(s.id);
-		//console.log(sp.id):
 		s.sendData = r.get({id:sp.id});
+
 		s.save = function(){
 			r.update({id:s.sendData.id},{data:s.sendData},function(data){
-				//console.log(data);
+
 				l.path('/trabajadores');
 			});
 		}
@@ -213,11 +177,11 @@ s.fincas = fr.query();
 		s.del = function(id){
 			r.delete({id:id}, function(datos){
 				console.log(datos);
-				//l.path('/trabajadores');
+				l.path('/trabajadores');
 			});
 		}
 	}]);
-
+	/*PREPLANILLA CONTROLLERS*/
 	app.controller('preplanilla',['$scope','prepResource','fincaResource', function(s,pr,fr){
 		s.prepSendData = {};
 		s.preplanillas = pr.query();
