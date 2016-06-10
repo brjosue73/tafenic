24d3cf0657db7ef28fa6f2f6d700de4560e4de98
Brandon Josué Blandón Vargas <Brandon Josu Blandn Vargas>
2016-06-10 00:34:11 -0600

    en teoria planilla de 15nal

--- app/Http/Controllers/QuincenalesController.php
+++ app/Http/Controllers/QuincenalesController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Quincenal
+use App\Variable;
 
 class QuincenalesController extends Controller
 {
-    public function quincenal(){
+    public function g_quincenal(){
+      $variables=Variable::all();
+      foreach ($variables as $variable) {
+        $inss_admin=$variable->inss_admin;
+
+      }
+
       $peticion = $request->all();
       $arreglo = $peticion["data"];
 
@@ -21,16 +28,28 @@ class QuincenalesController extends Controller
       $basico=$dias_trab*$salario_quinc;//menos los feriados y los subsidios, porque ya hay una caja de texto
 
 
-      $feriados1=$arreglo['feriado_trab'];//restarselo a los dias trabajados si no es trabajado
-      $feriados2=$arreglo['feriado_ntrab'];//cuenta como dia trabajado
+      $feriados1=$arreglo['feriado_trab'];//cuenta en los dias trab + valor del feriado
+      $feriados2=$arreglo['feriado_ntrab'];//no cuenta como dia trabajado
       $feriados=$feriados1+$feriados2;
       $feriado_tot=$feriados*$salario_dia;
       $planilla->feriado=$feriado_tot;
       $otros=$arreglo['otros'];
-      $subsidos=$arreglo['subsidos'];
+      $subsi=$arreglo['subsidos'];
+      $tot_sub=$subsi*$salario_dia;
       $horas_ext=$arreglo['horas_ext'];
       $tot_h_ext=$horas_ext*($sal_hora*2);
       $planilla->tot_h_ext=$tot_h_ext;
+      $devengado=$basico+$feriados+$otros+$subsidios+$tot_h_ext;
+      $inss_lab=($devengado-$subsidios)*$inss_admin;
+      $planilla->inss_lab=$inss_lab;
+      $IR=0;/*****************************FALTA CALCULAR IR**********************************************/
+      $prestamo=$arreglo['prestamos'];
+      $total_pagar=$devengado-$inss_laboral-$IR-$prestamo;
+      $inss_patronal=$devengado*18%;
+      $inatec=($devengado-$subsidios)*2%;
+      $planilla->total_pagar=$total_pagar;
+      $planilla->inss_patronal=$inss_patronal
+      $planilla->inatec=$inatec;
 
 
       $planilla->save();
