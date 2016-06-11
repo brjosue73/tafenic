b4ba5769fade0599df1cc89c5052cf65df561b5f
Brandon Josué Blandón Vargas <Brandon Josu Blandn Vargas>
2016-06-11 15:49:09 -0600

    repostes cat

---
+++ resources/views/reporte_catorcenal.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+REPORTE CATORCENAL
+@foreach ($data as $dat)
+  {{$dat['nombre']}}<br>
+@endforeach
+
+  <h1></h1>
+</body>
+</html>
