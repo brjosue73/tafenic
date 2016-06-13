f6c5ee6f155f2064c2ba1928224a4e85d4282598
J-Alex <jlex.wd@outlook.com>
2016-06-13 03:52:51 -0600

    lunes 13

--- resources/views/billetes_catorcenal.blade.php
+++ resources/views/billetes_catorcenal.blade.php
@@ -4,33 +4,44 @@
   <meta charset="UTF-8">
   <title>Distribucion de Billetes</title>
   <link rel="stylesheet" href="res/css/bootstrapTable.css">
+  <style>
+    .text-centro{
+      text-align: center;
+    }
+  </style>
 </head>
 <body>
+  <h5 class="text-centro">DISTRIBUCION DE BILLETES</h5>
   <div class="table-responsive">
     <table class="table table-striped table-bordered table-hover table-condensed">
       <thead>
         <tr class="active">
-          <th></th>
-          <th>C$ 500</th>
+          <th>SALARIO</th>
+          <th class="danger">C$ 500</th>
           <th>C$ 200</th>
-          <th>C$ 100</th>
-          <th>C$ 50</th>
-          <th>C$ 20</th>
-          <th>C$ 10</th>
+          <th class="info">C$ 100</th>
+          <th class="primary">C$ 50</th>
+          <th class="warning">C$ 20</th>
+          <th class="success">C$ 10</th>
           <th>C$ 5</th>
           <th>C$ 1</th>
         </tr>
       </thead>
       <tbody>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
+        @foreach($data as $dat)
+        <tr>
+          <td>{{ $dat['0'] }}</td>
+          <td class="danger">{{ $dat['500'] }}</td>
+          <td>{{ $dat['200'] }}</td>
+          <td class="info">{{ $dat['100'] }}</td>
+          <td class="primary">{{ $dat['50'] }}</td>
+          <td class="warning">{{ $dat['20'] }}</td>
+          <td class="success">{{ $dat['10'] }}</td>
+          <td>{{ $dat['5'] }}</td>
+          <td>{{ $dat['1'] }}</td>
+        </tr>
+        @endforeach
+
       </tbody>
     </table>
   </div>
