39ff11a9c69a119e0b8ffbda31e7b99779163537
J-Alex <jlex.wd@outlook.com>
2016-06-13 01:42:07 -0600

    reporte billete y sobre

--- resources/views/sobres_quincenal.blade.php
+++ resources/views/sobres_quincenal.blade.php
@@ -30,7 +30,7 @@
   <h5 class="text-centro">FINCA</h5>
   <h5 class="text-centro">PLANILLA DE PAGO DEL 27 ENERO 09 DE FEBRERO DEL 2016</h5>
   <table class="centrado">
-      <tr><th>{{ $dat['nombre'] }}</th></tr>
+      <tr><th></th></tr>
       <tr>
           <th>INGRESOS</th><th>-------</th>
           <th>DEDUCCIONES</th><th>-------</th>
@@ -38,63 +38,63 @@
     <tbody>
       <tr>
         <td> BASICO </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td> INSS </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
       </tr>
       <tr>
         <td> SEPTIMO </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td> PRESTAMOS </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
       </tr>
       <tr>
         <td> IR </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> N° HORAS EXTRAS </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> HORAS EXTRAS </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> FERIADO </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> VACACIONES </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> AGUINALDO </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td>  </td>
         <td>  </td>
       </tr>
       <tr>
         <td> OTROS </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td> TOTAL DEDUCCIONES </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
       </tr>
       <tr>
         <td> DEVENGADO </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
         <td> TOTAL A PAGAR </td>
-        <td> {{ $dat[''] }} </td>
+        <td>  </td>
       </tr>
     </tbody>
   </table>
