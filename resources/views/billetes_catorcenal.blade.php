929cf13e988260ffb02794dbb33b845fc10437a6
J-Alex <jlex.wd@outlook.com>
2016-06-13 02:53:20 -0600

    impresos y sobres2

---
+++ resources/views/billetes_catorcenal.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Distribucion de Billetes</title>
+  <link rel="stylesheet" href="res/css/bootstrapTable.css">
+</head>
+<body>
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover table-condensed">
+      <thead>
+        <tr class="active">
+          <th></th>
+          <th>C$ 500</th>
+          <th>C$ 200</th>
+          <th>C$ 100</th>
+          <th>C$ 50</th>
+          <th>C$ 20</th>
+          <th>C$ 10</th>
+          <th>C$ 5</th>
+          <th>C$ 1</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
--- resources/views/billetes_quincenal.blade.php
+++ resources/views/billetes_quincenal.blade.php
@@ -2,9 +2,37 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>Distribucion de Billetes</title>
+  <link rel="stylesheet" href="res/css/bootstrapTable.css">
 </head>
 <body>
-<h1>Billetes Quincenal en el rango de fechas </h1>
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover table-condensed">
+      <thead>
+        <tr class="active">
+          <th></th>
+          <th>C$ 500</th>
+          <th>C$ 200</th>
+          <th>C$ 100</th>
+          <th>C$ 50</th>
+          <th>C$ 20</th>
+          <th>C$ 10</th>
+          <th>C$ 5</th>
+          <th>C$ 1</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+      </tbody>
+    </table>
+  </div>
 </body>
 </html>
