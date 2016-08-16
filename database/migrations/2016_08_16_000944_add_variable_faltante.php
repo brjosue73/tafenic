7b2cec43276cbf12c7c1dcdcc8353694bc060557
J-Alex <jlex.wd@outlook.com>
2016-08-15 18:15:05 -0600

    problemas en la migracion de variables faltantes

---
+++ database/migrations/2016_08_16_000944_add_variable_faltante.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVariableFaltante extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('variables', function (Blueprint $table) {
+            $table->double('inss_patron');
+            $table->double('safa_peq');
+            $table->double('safa_grand');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('variables', function (Blueprint $table) {
+            //
+        });
+    }
+}
