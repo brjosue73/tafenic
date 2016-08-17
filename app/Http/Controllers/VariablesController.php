14198c37acbfc7a31d322e11dc2bc61321462d5e
J-Alex <jlex.wd@outlook.com>
2016-08-17 16:58:16 -0600

    inss patron varController from front

--- app/Http/Controllers/VariablesController.php
+++ app/Http/Controllers/VariablesController.php
@@ -93,7 +93,7 @@ class VariablesController extends Controller
       $variable->cuje_grand = $arreglo['cuje_grand'];
       $variable->hora_ext = $arreglo['hora_ext'];
       $variable->septimo = $arreglo['septimo'];
-      $variable->inss_patron = $arreglo['inss_pat'];
+      $variable->inss_patron = $arreglo['inss_patron'];
       $variable->safadura = $arreglo['safadura'];
       $variable->save();
       return "Registro Actualizado";
