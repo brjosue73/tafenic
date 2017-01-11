a980b8f8a1f1782cf988a7dedb19ad3c65bff40f
J-Alex <jlex.wd@outlook.com>
2017-01-10 22:57:03 -0600

    fixing de media noche pinche reporte

--- resources/views/reporte_catorcenal.blade.php
+++ resources/views/reporte_catorcenal.blade.php
@@ -142,13 +142,13 @@
             <td>{{number_format( $totales['sum_feriados'],2 ) }}</td>
             <td>{{number_format( $totales['sum_dev2'],2 ) }}</td>
             <td>{{number_format( $totales['sum_h_ext'],2 ) }}</td>
-            <td>{{number_format( $totales['sum_tot_hext'],2 ) }}</td>
-            <td>{{number_format( $totales['sum_tot_hext'],2 ) }}</td>
             <td>0</td>
+            <!-- <td>{{number_format( $totales['sum_tot_hext'],2 ) }}</td> -->
+            <td>{{number_format( $totales['sum_tot_hext'],2 ) }}</td>
             <td>{{number_format( $totales['sum_vacs'],2 ) }}</td>
             <td>{{number_format( $totales['sum_aguin'],2 ) }}</td>
             <td>{{number_format( $totales['sum_acum'],2 ) }}</td>
-            <!-- <td>{{number_format( $totales['sum_inss_lab'],2 ) }}</td> -->
+            <td>{{number_format( $totales['sum_inss_lab'],2 ) }}</td>
             <td>{{number_format( $totales['sum_prestam'],2 ) }}</td>
             <td>{{number_format( $totales['sum_tot_recib'],2 ) }}</td>
             <td></td>
