8604add1ac41cba9089ce03d3706be245505553f
brjosue73 <brjosue73@gmail.com>
2017-01-19 12:37:21 -0500

    fix

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,8 @@ return array(
         'enabled' => true,
         //'binary'  => '/usr/local/bin/wkhtmltopdf',
         //'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        //'binary' => base_path('vendor/wemersonjanuario/wkhtmltopdf-windows/bin/64bit/wkhtmltopdf'),
+	'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
 
 
         'timeout' => false,
@@ -15,8 +16,8 @@ return array(
     'image' => array(
         'enabled' => true,
         //'binary'  => '/usr/local/bin/wkhtmltoimage',
-        //'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage'),
+        'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
+        //'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage'),
 
 
         'timeout' => false,
