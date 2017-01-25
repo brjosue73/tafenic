45063fb6f03045e788118c8bb0acb208d33e4455
Brandon Josué Blandón Vargas <Brandon Josu Blandn Vargas>
2017-01-25 12:16:07 -0600

    fix pdf activ

--- app/Http/Controllers/PlanillasController.php
+++ app/Http/Controllers/PlanillasController.php
@@ -68,7 +68,7 @@ class PlanillasController extends Controller
     $prestamos=$totales['sum_prestam'];
     $alim=$totales['sum_alim'];
     $tot_hext=$totales['sum_tot_hext'];
-
+    $dinero_cuje=$totales['sum_dinero_activ'];
 
     $a_vac=$dev+$septimo+$feriados;
     $vacs=$a_vac*0.083333;
