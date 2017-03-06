ad79e27b145209f50c462073bce917342a79c7a3
J-Alex <jlex.wd@outlook.com>
2017-03-06 15:46:55 -0600

    la variable

--- resources/views/sobres_catorcenal.blade.php
+++ resources/views/sobres_catorcenal.blade.php
@@ -97,7 +97,7 @@
   $i=0;
    ?>
   @foreach($data as $dat)
-  <h4 class="text-centro">TABACALERA FERNANDEZ DE NICARAGUA S. A. <span class="numeracion">#</span></h4>
+  <h4 class="text-centro">TABACALERA FERNANDEZ DE NICARAGUA S. A. <span class="numeracion"> {{ $i++ }} </span></h4>
   <h5 class="text-centro">FINCA</h5>
   <h5 class="text-centro">PLANILLA DE PAGO DEL {{$fecha_1}} al {{$dat['fecha_fin']}}</h5>
   <table>
